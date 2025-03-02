c3911ccb7dd90856c342abafa73befa69cf8de8c
Mamta Jangra <mamtajangra204@gmail.com>
2025-03-02 15:20:48 +0530

    Refactor bot.py to improve message sending functionality and enhance threading management

--- bot.py
+++ bot.py
@@ -1,30 +1,40 @@
 import os
 import requests
 import threading
-
 import dotenv
 
+# Load environment variables from a .env file
 dotenv.load_dotenv()
 
+# Get the bot token from environment variables
 TOKEN = os.getenv("BOT_TOKEN")
 
-text_send ="hello python"
-chat_id=1878669270
+# Message to be sent
+text_send = "hello python"
 
+# List of user IDs to send messages to
 list_of_users = [1878669270, 325011602]
-def send_message(chat_id,text_send):
-    response = requests.get(f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={text_send}"
-    )
+
+# Function to send a message to a specific chat ID
+def send_message(chat_id, text_send):
+    # Make a GET request to the Telegram API to send a message
+    response = requests.get(f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={text_send}")
+    # Print the response from the API
     print(response.json())
 
+# List to keep track of threads
+list_of_threads = []
 
-
-list_of_threads=[]
+# Create and start threads for each user ID
 for userid in list_of_users:
     for i in range(10):
-        g=threading.Thread(target=send_message,args=(userid,text_send))
+        # Create a new thread to send a message
+        g = threading.Thread(target=send_message, args=(userid, text_send))
+        # Add the thread to the list of threads
         list_of_threads.append(g)
+        # Start the thread
         g.start()
 
+# Wait for all threads to complete
 for thread in list_of_threads:
     thread.join()
